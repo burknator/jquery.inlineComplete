3d06529763c2b307ebfdbbdc8cad3e7aea03ecab
Patrick Burke <patrick@patrickburke.de>
2011-03-02 17:34:15 +0100

    Added support for setting the select range of multiple elements in a jQuery object

--- jquery.inlinecomplete.js
+++ jquery.inlinecomplete.js
@@ -27,26 +27,24 @@
 	}
 	
 	sub.fn.select = function(startPos, endPos) {
-		if(!this.is('input[type=text]'))
-			return this;
-		
 		if (startPos && endPos) {
-			var input = this.get(0);
-			
-			if (typeof input.selectionStart != "undefined") {
-				input.selectionStart = startPos;
-				input.selectionEnd = endPos;
-			}
-			else if (document.selection && document.selection.createRange) {
-				// IE branch
-				input.focus();
-				input.select();
-				var range = document.selection.createRange();
-				range.collapse(true);
-				range.moveEnd("character", endPos);
-				range.moveStart("character", startPos);
-				range.select();
-		 	}
+			// No filtering done here since we're checking the existence of selectionStart/-End and select().
+			this.each(function() {
+				if (typeof this.selectionStart != "undefined") {
+					this.selectionStart = startPos;
+					this.selectionEnd = endPos;
+				}
+				else if (document.selection && document.selection.createRange && this.select) {
+					// IE branch
+					this.focus();
+					this.select();
+					var range = document.selection.createRange();
+					range.collapse(true);
+					range.moveEnd("character", endPos);
+					range.moveStart("character", startPos);
+					range.select();
+			 	}
+			});
 		}
 		
 		return this;
